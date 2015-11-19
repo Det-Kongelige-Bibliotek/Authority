1e397811a6dffd3a9f4af66eb57f2d107785fa96
David Grove Jørgensen <dgj@kb.dk>
2015-11-19 14:59:03 +0100

    added simple view and controller for person

--- app/controllers/authority/people_controller.rb
+++ app/controllers/authority/people_controller.rb
@@ -1,9 +1,21 @@
 module Authority
    class PeopleController < ApplicationController
+     before_action :set_person, only: [:show, :edit, :update, :destroy]
+
      def index
-        render plain: 'Hello'
-	response.body # => "Hello from people controller"
-	response.content_type # => "text/plain"
+       @people = Authority::Person.all
+     end
+
+     def show
+     end
+
+     def edit
+     end
+
+     private
+
+     def set_person
+       @person = ActiveFedora::Base.where(id: URI.unescape(params[:id])).first
      end
    end
 end
