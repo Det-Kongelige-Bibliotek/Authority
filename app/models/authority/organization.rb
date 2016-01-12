ba14c02180520de90d775ae70e571cd5e34a7374
Nader <nkh@kb.dk>
2016-01-12 10:25:49 +0100

    Organization controller and model

--- app/models/authority/organization.rb
+++ app/models/authority/organization.rb
@@ -1,5 +1,14 @@
 module Authority
   class Organization < ::ActiveFedora::Base
     include ::Authority::Concern::Thing
+
+    property :address, predicate: ::RDF::Vocab::SCHEMA.address, multiple: false
+    property :contact_point, predicate: ::RDF::Vocab::SCHEMA.contactPoint, multiple: true
+    property :department, predicate: ::RDF::Vocab::SCHEMA.department, multiple: false
+    property :email, predicate: ::RDF::Vocab::SCHEMA.email, multiple: true do |index|
+      index.as :stored_searchable
+    end
+    property :globbal_location_number, predicate: ::RDF::Vocab::SCHEMA.globalLocationNumber, multiple: false
+    property :telephone, predicate: ::RDF::Vocab::SCHEMA.telephone, multiple: true
   end
 end
