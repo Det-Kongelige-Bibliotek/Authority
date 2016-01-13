2b9a1ec52bb53f927280d24ba7a5e2f4e6efbf6f
David Grove Jørgensen <dgj@kb.dk>
2016-01-13 12:15:58 +0100

    added typeahead js

---
+++ app/assets/javascripts/authority/authority_typeahead.js
@@ -0,0 +1,72 @@
+(function($) {
+    $.fn.extend({
+        authority_typeahead: function(options) {
+            options = $.extend( {}, $.authority_typeahead.defaults, options );
+
+            this.each(function() {
+                new $.authority_typeahead(this,options);
+            });
+            return this;
+        }
+    });
+
+    $.authority_typeahead = function( ctl, options ) {
+
+        if (options.remove) {
+          typeahead_field = $(ctl).next(".twitter-typeahead").find("#"+ctl.id + "_typeahead");
+          typeahead_field.typeahead("destroy");
+          typeahead_field.remove();
+        } else {
+            typeahead_field = $(ctl).clone().attr("id", ctl.id + "_typeahead").removeAttr("name");
+
+            typeahead_field.insertAfter(ctl);
+
+            var localagents = new Bloodhound({
+                datumTokenizer: Bloodhound.tokenizers.whitespace,
+                queryTokenizer: Bloodhound.tokenizers.whitespace,
+                limit: 10,
+                remote: {
+                    url: '/authority/finder/search/' + options.model + '/%QUERY',
+                    wildcard: '%QUERY'
+                }
+            });
+
+            typeahead_field.typeahead({
+                    minLength: 1,
+                    highlight: true
+                },
+                {
+                    name: 'Agents',
+                    source: localagents,
+                    displayKey: 'val',
+                    templates: {
+                        header: '<h3 class="agent-source">Valhal</h3>',
+                        empty: '<div class="empty-message">Ingen agenter fundet. Opret evt. en ny</div>'
+                    }
+                }).bind('typeahead:select', function (event, suggestion) {
+                    $("#" + ctl.id).val(suggestion.id);
+                });
+
+            // update the typeahead field
+
+            if ($(ctl).val() != "") {
+                $.ajax({
+                    type: 'GET',
+                    async: false,
+                    url: '/authority/finder/getobj/' + encodeURIComponent($(ctl).val()),
+                    dataType: 'json',
+                    success: function (data) {
+                        typeahead_field.val(data.val);
+                    }
+                });
+            }
+
+        }
+    };
+
+    $.authority_typeahead.defaults = {
+        remove: false,
+        model: "all"
+    };
+
+})(jQuery);
--- app/assets/javascripts/authority/person.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $('[data-function="clone-input"]').click(function () {
-    $input = $(this).prev('input');
-    $input.clone().insertAfter($input);
-  })
-});
