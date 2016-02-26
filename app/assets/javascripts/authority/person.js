a1e6129d5d05e8a36f7847aabdce94a282fa5155
Polyxeni Makri <poma@kb.dk>
2016-02-26 09:47:14 +0100

    Function to fill in the given and family name with 'unknown'

--- app/assets/javascripts/authority/person.js
+++ app/assets/javascripts/authority/person.js
@@ -2,14 +2,19 @@ $(document).ready(function () {
   $('[data-function="clone-input"]').click(function () {
     $input = $(this).prev('input');
     $input.clone().insertAfter($input);
-  })
+  });
 
-  $()
-    if (email) {
-       if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-           text = text + "Emailadressen er ugyldigt\n";
-           error = 1;
-       }
-    }
+  //$()
+  //  if (email) {
+  //     if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+  //         text = text + "Emailadressen er ugyldigt\n";
+  //         error = 1;
+  //     }
+  //  }
 
 });
+// Fill the fields in the form with 'unknown'
+function fill_unknown_fields() {
+    $('#person_given_name').val('unknown');
+    $('#person_family_name').val('unknown');
+}
